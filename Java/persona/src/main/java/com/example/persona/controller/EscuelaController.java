1bea1bdb1f47e980b85f1f991b2d148a61c01b36
mesafrancisco <145351124+mesafrancisco@users.noreply.github.com>
2024-04-27 20:58:03 +0200

    Clase Escuela Creada Y Funcionando

--- Java/persona/src/main/java/com/example/persona/controller/EscuelaController.java
+++ Java/persona/src/main/java/com/example/persona/controller/EscuelaController.java
@@ -1,6 +1,5 @@
 package com.example.persona.controller;
 
-
 import java.util.List;
 import java.util.Optional;
 
@@ -18,61 +17,61 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.persona.entity.Escuela;
 import com.example.persona.entity.Persona;
+import com.example.persona.repository.EscuelaRepository;
 import com.example.persona.repository.PersonaRepository;
 
 @RestController
-@RequestMapping("/personas")
-public class PersonaController {
+@RequestMapping("/escuelas")
+public class EscuelaController {
 	
-	private final PersonaRepository personaRepository;
-
-	private PersonaController(PersonaRepository personaRepository) {
-		this.personaRepository = personaRepository;
+	private final EscuelaRepository escuelaRepository;
+	
+	private EscuelaController(EscuelaRepository escuelaRepository) {
+		this.escuelaRepository = escuelaRepository;
 	}
-//	frases bonitas y fotos de marte 
-
+	
 	@GetMapping
-	private ResponseEntity<List<Persona>> findAll(Pageable pageable) {
-		Page<Persona> page = personaRepository.findAll(PageRequest.of(pageable.getPageNumber(), // 0
+	private ResponseEntity<List<Escuela>> findAll(Pageable pageable) {
+		Page<Escuela> page = escuelaRepository.findAll(PageRequest.of(pageable.getPageNumber(), // 0
 				pageable.getPageSize(), // 20
 				pageable.getSortOr(Sort.by(Sort.Direction.ASC, "nombre"))));
 		return ResponseEntity.ok(page.getContent());
 	}
 
 	@GetMapping("/{requestedId}")
-	private ResponseEntity<Persona> findById(@PathVariable Long requestedId) {
-		Optional<Persona> personaOptional = personaRepository.findById(requestedId);
-		if (personaOptional.isPresent()) {
-			return ResponseEntity.ok(personaOptional.get());
+	private ResponseEntity<Escuela> findById(@PathVariable Long requestedId) {
+		Optional<Escuela> escuelaOptional = escuelaRepository.findById(requestedId);
+		if (escuelaOptional.isPresent()) {
+			return ResponseEntity.ok(escuelaOptional.get());
 		} else {
 			return ResponseEntity.notFound().build();
 		}
 	}
 
 	@PostMapping
-	private ResponseEntity<Persona> createPersona(@RequestBody Persona newPersonaRequested) {
-		Persona savedPersona = personaRepository.save(newPersonaRequested);
-		return ResponseEntity.ok(savedPersona);
+	private ResponseEntity<Escuela> createEscuela(@RequestBody Escuela newEscuelaRequested) {
+		Escuela savedEscuela = escuelaRepository.save(newEscuelaRequested);
+		return ResponseEntity.ok(savedEscuela);
 	}
 
 	@PutMapping("/{id}")
-	private ResponseEntity<Void> updatePersona(@PathVariable Long id, @RequestBody Persona updatedPersona) {
-		if (!personaRepository.existsById(id)) {
+	private ResponseEntity<Void> updateEscuela(@PathVariable Long id, @RequestBody Escuela updatedEscuela) {
+		if (!escuelaRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
 		}
-		updatedPersona.setId(id);
-		personaRepository.save(updatedPersona);
+		updatedEscuela.setId(id);
+		escuelaRepository.save(updatedEscuela);
 		return ResponseEntity.noContent().build();
 	}
 
 	@DeleteMapping("/{id}")
-	private ResponseEntity<Void> deletePersona(@PathVariable Long id) {
-		if (!personaRepository.existsById(id)) {
+	private ResponseEntity<Void> deleteEscuela(@PathVariable Long id) {
+		if (!escuelaRepository.existsById(id)) {
 			return ResponseEntity.notFound().build();
 		}
-		personaRepository.deleteById(id);
+		escuelaRepository.deleteById(id);
 		return ResponseEntity.noContent().build();
 	}
-
 }
--- Java/persona/src/main/java/com/example/persona/repository/EscuelaRepository.java
+++ Java/persona/src/main/java/com/example/persona/repository/EscuelaRepository.java
@@ -1,11 +1,13 @@
 package com.example.persona.repository;
-import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
-import com.example.persona.entity.Persona;
-
+import com.example.persona.entity.Escuela;
 @Repository
-public interface PersonaRepository extends CrudRepository<Persona, Long>, PagingAndSortingRepository<Persona, Long> {
+public interface EscuelaRepository extends CrudRepository<Escuela, Long>,PagingAndSortingRepository<Escuela,Long>{
 	
 }
+
+
+
