eac29a96d2ebf28eb9e1075b7660e278cb49997e
mesafrancisco <145351124+mesafrancisco@users.noreply.github.com>
2024-05-03 16:09:34 +0200

    Borro parte de Android, Comienzo de nuevo

--- Android/app/src/main/java/com/example/myapppersonas/Constantes.kt
+++
@@ -1,6 +0,0 @@
-package com.example.myapppersonas
-
-object Constantes {
-    //Creamos constante con la url de mi maquina. POSIBLE FALLO SI NO FUNCIONA.
-    const val BASE_URL = "http://127.0.0.1:8080"
-}
--- Android/app/src/main/java/com/example/myapppersonas/WebService.kt
+++
@@ -1,4 +0,0 @@
-package com.example.myapppersonas
-
-interface WebService {
-}
